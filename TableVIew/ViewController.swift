4ccaa27f1ccbf55edc7f1d483a39ae8e7d66132e
Kasyhyap <kashyap.bhatt@moveinsync.com>
2020-08-05 22:11:54 +0530

    Initial Commit

---
+++ TableVIew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableVIew
+//
+//  Created by Kashyap on 05/08/20.
+//  Copyright © 2020 Kashyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
