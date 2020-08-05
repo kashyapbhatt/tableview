0061a0d90f9b3937b2263bdaefec20873e6a29ad
Kasyhyap <kashyap.bhatt@moveinsync.com>
2020-08-05 23:34:08 +0530

    first commit

---
+++ TableVIew/ImageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ImageTableViewCell.swift
+//  TableVIew
+//
+//  Created by Kashyap on 05/08/20.
+//  Copyright © 2020 Kashyap. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TableVIew/TextTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextTableViewCell.swift
+//  TableVIew
+//
+//  Created by Kashyap on 05/08/20.
+//  Copyright © 2020 Kashyap. All rights reserved.
+//
+
+import UIKit
+
+class TextTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
